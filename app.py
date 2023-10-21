0dab03bae6877a8c4cf8611738e81072fe374c83
giofile <monsanto.g@chello.nl>
2023-10-21 20:45:35 +0200

    updated first commit

--- app.py
+++ app.py
@@ -3,34 +3,50 @@ import secrets
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
-tasks = {}
+task = {}
+task_file = 'tasks.txt'
 
 
 def get_tasks():
-    if 'tasks' not in g:
-        g.tasks = {}
-    return g.tasks
+    try:
+        with open(task_file, 'r') as file:
+            tasks = file.read().splitlines()
+        return tasks
+    except FileNotFoundError:
+        return []
+
+
+def save_tasks(tasks):
+    with open(task_file, 'w') as file:
+        file.write('\n'.join(tasks))
 
 
 @app.route('/')
 def index():
+    tasks = get_tasks()
     return render_template('index.html', tasks=tasks)
 
 
 @app.route('/add', methods=['POST'])
-def add_task():
-    task_name = request.form['task']
-    if task_name not in tasks:
-        tasks[task_name] = None
-    return render_template('index.html', tasks=tasks)
+def add():
+    new_task = request.form['task']
+    tasks = get_tasks()
+    tasks.append(new_task)
+    save_tasks(tasks)
+    return redirect(url_for('index'))
 
 
 @app.route('/remove', methods=['POST'])
-def remove_task():
-    task_name_to_remove = request.form['task']
-    if task_name_to_remove in tasks:
-        del tasks[task_name_to_remove]
-    return render_template('index.html', tasks=tasks)
+def remove():
+    task_to_remove = request.form['task']
+    tasks = get_tasks()
+
+    if task_to_remove in tasks:
+        tasks.remove(task_to_remove)
+        save_tasks(tasks)
+
+    return redirect('/')
+
 
 # # This block is necessary when you run the app directly (not during testing)
 # if __name__ == '__main__':
