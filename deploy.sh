70ff61ee84b970c6887e0c456287cb8417faee56
giofile <monsanto.g@chello.nl>
2023-10-25 02:47:40 +0200

    large update

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,12 @@
 
 # SSH Connection Details
 SSH_USER="root"
-SERVER_IP="167.99.32.43"
+SERVER_IP="64.225.64.147" 
 SSH_PORT="22"
 
 # Application Details
 APP_DIR="/root/ToDoApp"
 APP_START_COMMAND="gunicorn -w 4 -b 0.0.0.0:5000 app:app"
-
-# GitHub Repository Details (Not needed in this script, used in GitHub Actions workflow)
-GITHUB_REPO="https://github.com/giofile/flask-todo-app.git"
-BRANCH="main"
 
 # Deployment Script
 echo "Deploying Flask Application to Digital Ocean..."
